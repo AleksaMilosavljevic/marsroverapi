e0ba7d2553f25d6a1141fb8cf8d269b8a7980ebb
Aleksa Milosavljevic <aleksa.milosavljevic1998@gmail.com>
2023-04-16 15:59:46 +0200

    Added frontend with bootstrap and thymeleaf

--- src/main/java/com/aleksa/marsroverapi/web/HomeController.java
+++ src/main/java/com/aleksa/marsroverapi/web/HomeController.java
@@ -1,16 +1,25 @@
 package com.aleksa.marsroverapi.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.aleksa.marsroverapi.response.MarsRoverApiResponse;
+import com.aleksa.marsroverapi.service.MarsRoverApiService;
 
 
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private MarsRoverApiService roverService;
     
     @GetMapping("/")
     public String getHome(ModelMap model){
+        MarsRoverApiResponse roverData = roverService.getRoverData();
+        model.put("roverData", roverData);
         return "index";
     }
 }
