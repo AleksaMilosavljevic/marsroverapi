20ee958cd7d206a2918fbc66314357a055ad7258
Aleksa Milosavljevic <aleksa.milosavljevic1998@gmail.com>
2023-05-01 22:58:52 +0200

    Refactoring ugly code

--- src/main/java/com/aleksa/marsroverapi/service/MarsRoverApiService.java
+++ src/main/java/com/aleksa/marsroverapi/service/MarsRoverApiService.java
@@ -1,22 +1,62 @@
 package com.aleksa.marsroverapi.service;
 
-import org.springframework.http.ResponseEntity;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.aleksa.marsroverapi.dto.HomeDto;
+import com.aleksa.marsroverapi.response.MarsPhoto;
 import com.aleksa.marsroverapi.response.MarsRoverApiResponse;
 
 @Service
 public class MarsRoverApiService {
 
    // private static final String API_KEY = "";
+
+   private Map<String, List<String>> validCameras = new HashMap<>();
+
+   public MarsRoverApiService(){
+    validCameras.put("Opportunity", Arrays.asList("FHAZ","RHAZ","NAVCAM","PANCAM","MINITES"));
+    validCameras.put("Curiosity",Arrays.asList("FHAZ","RHAZ","MAST","CHEMCAM","MHALI","MARDI","NAVCAM"));
+    validCameras.put("Spirit", Arrays.asList("FHAZ","RHAZ","NAVCAM","PANCAM","MINITES"));
+   }
   
-    public MarsRoverApiResponse getRoverData(String roverType, Integer marsSol){
+    public MarsRoverApiResponse getRoverData(HomeDto homeDto) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         RestTemplate rt = new RestTemplate();
 
-        ResponseEntity<MarsRoverApiResponse> response = 
-         rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverType+"/photos?sol="+marsSol+"&api_key=DEMO_KEY" , MarsRoverApiResponse.class);
+        List<String> apiUrlEndpoints = getApiUrlEndpoints(homeDto);
+        List<MarsPhoto> marsPhotos = new ArrayList<>();
+        MarsRoverApiResponse response = new MarsRoverApiResponse();
+        apiUrlEndpoints.stream()
+                        .forEach(url -> {
+                            MarsRoverApiResponse apiResponse = rt.getForObject(url, MarsRoverApiResponse.class);
+                            marsPhotos.addAll(apiResponse.getPhotos());
+                        });
+       
+        response.setPhotos(marsPhotos);                
+        return response;
+    }
 
-        return response.getBody();
+    public List<String> getApiUrlEndpoints (HomeDto homeDto) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        List<String> urls = new ArrayList<>();
+
+        Method[] methods = homeDto.getClass().getMethods();
+
+        for (Method method : methods) {
+           if(method.getName().indexOf("getCamera")> -1  && Boolean.TRUE.equals(method.invoke(homeDto))) {
+                String cameraName = method.getName().split("getCamera")[1].toUpperCase();
+                if(validCameras.get(homeDto.getMarsApiRoverData()).contains(cameraName)) {
+                    urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/"+homeDto.getMarsApiRoverData()+"/photos?sol="+homeDto.getMarsSol()+"&api_key=DEMO_KEY" + "&camera="+ cameraName);
+                }
+           }
+        }
+        return urls;
     }
 }
--- src/main/java/com/aleksa/marsroverapi/web/HomeController.java
+++ src/main/java/com/aleksa/marsroverapi/web/HomeController.java
@@ -1,11 +1,11 @@
 package com.aleksa.marsroverapi.web;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aleksa.marsroverapi.dto.HomeDto;
 import com.aleksa.marsroverapi.response.MarsRoverApiResponse;
@@ -22,14 +22,14 @@ public class HomeController {
     private MarsRoverApiService roverService;
     
     @GetMapping("/")
-    public String getHome(ModelMap model,HomeDto homeDto){
+    public String getHome(ModelMap model,HomeDto homeDto) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         if(StringUtils.isEmpty(homeDto.getMarsApiRoverData())){
             homeDto.setMarsApiRoverData("Opportunity");
         }
         if(homeDto.getMarsSol() == null){
             homeDto.setMarsSol(1);
         }
-        MarsRoverApiResponse roverData = roverService.getRoverData(homeDto.getMarsApiRoverData(),homeDto.getMarsSol());
+        MarsRoverApiResponse roverData = roverService.getRoverData(homeDto);
         model.put("roverData", roverData);
         model.put("homeDto", homeDto);
         return "index";
